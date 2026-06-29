b25f803c3a98daddc8a7b634f8be737b6fe96fb9
Subik <subik@example.com>
2026-06-29 17:39:36 +0530

    Fix PM2 logging permission issue and interpreter path

--- deploy/ecosystem.config.js
+++ deploy/ecosystem.config.js
@@ -5,15 +5,13 @@ module.exports = {
       script: "gunicorn",
       args: "--bind 127.0.0.1:8000 --workers 4 --timeout 300 backend.wsgi:application",
       cwd: "/home/ubuntu/finprince/backend",
-      interpreter: "python3", // or point to your virtualenv like "/home/ubuntu/finprince/venv/bin/python"
+      interpreter: "/home/ubuntu/finprince/backend/venv/bin/python",
       env: {
         DJANGO_SETTINGS_MODULE: "backend.settings",
         DJANGO_DEBUG: "False", // Ensure False in production
         CLUSTER_ENV: "production",
       },
       log_date_format: "YYYY-MM-DD HH:mm Z",
-      error_file: "/var/log/pm2/finprince-backend-error.log",
-      out_file: "/var/log/pm2/finprince-backend-out.log",
       merge_logs: true,
       autorestart: true,
       max_restarts: 10,
